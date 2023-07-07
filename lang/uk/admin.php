58b92ae285204dfee064349d0dc583c2d085631f
Vlad Hrytsyshyn <61755593+xlanyleeet@users.noreply.github.com>
2023-07-07 10:56:03 +0300

    add uk lang
    add uk lang

---
+++ lang/uk/admin.php
@@ -0,0 +1,63 @@
+<?php
+return [
+
+    'none' => 'Нічого',
+    'title' => 'Заголовок',
+    'picture' => 'Зображення',
+    'text' => 'Текст',
+    'paragraph' => 'Параграф',
+    'url' => 'Посилання',
+    'icon' => 'Іконка',
+    'color' => 'Колір',
+    'colors' => 'Кольори',
+    'secondary' => 'Другорядний',
+    'primary' => 'Основний',
+    'main' => 'Головний',
+    'body' => 'Тіло',
+    'discord' => 'Discord',
+    'twitter' => 'Twitter',
+    'server' => 'Сервер',
+    'userBar' => 'Панель користувача',
+    'hero' => 'Топ панель',
+    'show' => 'Показати',
+    'add' => 'Додати',
+    'dont_show' => 'Не показувати',
+    'button' => 'Кнопка',
+    'theme' => 'Тема',
+    'black' => 'Чорний',
+    'white' => 'Білий',
+    'footer' => 'Футер',
+    'middle' => 'Середній',
+    'left' => 'Ліво',
+    'right' => 'Право',
+    'other_page' => 'Відкрити на іншій сторінці',
+    'ip_when_copied' => 'Текст, коли айпі скопійовано',
+    'online' => 'Онлайн',
+    'join' => 'Приєднатись',
+    'amount_to_show' => 'Сума для показу',
+    'online_variable' => 'Використовуйте {online} у вашому тексті, щоб розмістити кількість гравців!',
+    'form' => [
+        'hero' => [
+            'discord_show_online' => 'Налаштуйте свій Discord ID нижче, щоб відобразити кількість підключень, які ви маєте.',
+            'discord_iframe' => 'На жаль, ви не можете використовувати {online}, ви використовуєте налаштування iframe.'
+        ],
+        'colors' => [
+            'desc' => 'Додайте випадкові кольори, щоб мати більше знань про кольори, які змінюються',
+            'color_of_primary_buttons' => 'Колір тексту для основних кнопок',
+            'color_of_secondary_buttons' => 'Колір тексту для другорядних кнопок',
+        ],
+        'color' => [
+            'default_color' => 'Стандартний колір',
+        ],
+        'footer' => [
+            'showing_serveurliste' => 'Не показувати сервер на serveurliste.com',
+            'link_of_your_serveur_on_serveurliste' => 'Посилання на вашу сторінку Serveurliste. <a href="https://serveurliste.com/faq" target="_blank">Документація</a>'
+        ],
+    ],
+    'config' => [
+        'our_discord' => 'Наш Discord',
+        'serveurliste' => 'Перелічіть свої сервери на serveurliste.com',
+        'don' => 'Зробити пожертву',
+        'donation' => 'Ми пишаємося вашою підтримкою і дуже цінуємо ваш щедрий внесок, Dixept ❤ .',
+    ],
+];
