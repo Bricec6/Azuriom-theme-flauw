2d68c6ad789aaf8643131cfd6f2cf59c6c23eeba
Bricec6 <bricec432@gmail.com>
2024-06-21 20:33:10 +0200

    init

--- views/home.blade.php
+++ views/home.blade.php
@@ -47,6 +47,58 @@
     </div>
 
     <div class="bg-body content">
+
+        @if(! $servers->isEmpty())
+            <div class="container px-0 pt-5">
+                <h2 class="text-uppercase mb-4">
+                    {{ trans('messages.servers') }}
+                </h2>
+
+                <div class="row gy-3 mb-4">
+                    @foreach($servers as $server)
+                        <div class="col-md-4">
+                            <div class="card h-100">
+                                <div class="card-body">
+                                    <h3 class="card-title mb-3">
+                                        {{ $server->name }}
+                                    </h3>
+
+                                    @if($server->isOnline())
+                                        <div class="progress mb-1">
+                                            <div class="progress-bar" role="progressbar" style="width: {{ $server->getPlayersPercents() }}%">
+                                            </div>
+                                        </div>
+
+                                        <p class="mb-1">
+                                            {{ trans_choice('messages.server.total', $server->getOnlinePlayers(), [
+                                                'max' => $server->getMaxPlayers(),
+                                            ]) }}
+                                        </p>
+                                    @else
+                                        <p>
+                                    <span class="badge bg-danger">
+                                        {{ trans('messages.server.offline') }}
+                                    </span>
+                                        </p>
+                                    @endif
+
+                                    <div class="text-end">
+                                        @if($server->join_url)
+                                            <a href="{{ $server->join_url }}" class="btn btn-primary">
+                                                {{ trans('messages.server.join') }}
+                                            </a>
+                                        @else
+                                            <p class="card-text">{{ $server->fullAddress() }}</p>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        @endif
+
         <div class="container">
             @include('elements.session-alerts')
 
@@ -143,7 +195,7 @@
                         </div>
                         <div class="bg-dark bg-opacity-10 p-md-4">
                             <div class="row gy-3 m-0">
-                                <p>{{trans('theme::theme.post.add_news')}}</p>
+                                <p>{{trans('theme::theme.posts.add_news')}}</p>
                             </div>
                         </div>
                     @endif
